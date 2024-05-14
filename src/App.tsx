9e83e3f155adda5cd1ddfaf0e702f25036f6d813
MAYCON PACHECO <151109977+mayconpachecooficial@users.noreply.github.com>
2024-05-14 19:14:32 -0400

    Update

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import './App.scss'
+
+function App() {
+  return (
+    <main>
+      <header className='header-container'>
+        <div className='logo-container'>
+          <img src="./logo-img" />
+          <div className='logo-text'>
+            <span className='logo-text-bold'>summerly</span>
+            <span className='logo-text-regular'>media</span>
+          </div>{/* logo-text */}
+        </div>{/* logo-container */}
+      </header>{/* header-container */}
+    </main>
+  )
+}
+
+export default App
